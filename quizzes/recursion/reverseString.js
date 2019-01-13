def599dd7c628afc6c51cc86f069f042fc1acbd8
Nathan <nathanarohde@gmail.com>
2019-01-12 16:29:50 -0800

    Actual solution to reverse string problem.

--- quizzes/recursion/reverseString.js
+++ quizzes/recursion/reverseString.js
@@ -14,19 +14,28 @@
 // even or odd?
 // a string that maintains length can not be passed as a base condition as there is nothing to track
 // recursive
-function reverse(string){
-  let counter = Math.floor(string.length/2);
-  let stringArray = string.split("");
+// function reverse(string){
+//   let counter = Math.floor(string.length/2);
+//   let stringArray = string.split("");
+//
+//   function iterate(stringArray){
+//     if (counter <= 0) return stringArray.join("");
+//     let temp = stringArray[counter - 1];
+//     stringArray[counter - 1] = stringArray[string.length - counter];
+//     stringArray[string.length - counter] = temp;
+//     counter--;
+//     return iterate(stringArray);
+//   }
+//
+//   return iterate(stringArray);
+// }
 
-  function iterate(stringArray){
-    if (counter <= 0) return stringArray.join("");
-    let temp = stringArray[counter - 1];
-    stringArray[counter - 1] = stringArray[string.length - counter];
-    stringArray[string.length - counter] = temp;
-    counter--;
-    return iterate(stringArray);
-  }
-
-  return iterate(stringArray);
+// works because of how the call stack works
+// removes first character a places it at end
+// actual solution
+function reverse(str){
+	if (str.length <= 1) return str;
+  // console.log((str.slice(1)) + str[0]);
+	return reverse(str.slice(1)) + str[0];
 }
 console.log(reverse('awesome'));
