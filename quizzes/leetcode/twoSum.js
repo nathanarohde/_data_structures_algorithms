05627267c5bc93c87377780b197237397ac942ec
Nathan <nathanarohde@gmail.com>
2019-01-13 12:41:01 -0800

    restored non-minified code
    Solution is better but still not optimal.

--- quizzes/leetcode/twoSum.js
+++ quizzes/leetcode/twoSum.js
@@ -1,4 +1,4 @@
-// mapArray[complement][j]
+// c[d][j]
 var twoSum = function(nums, target) {
   let mapArray = {}
   for (let i = 0; i < nums.length; i++){
@@ -20,5 +20,23 @@ var twoSum = function(nums, target) {
   }
   return false;
 };
+//not quite optimal solution
+
+// minified shaves 4ms?
+// var twoSum = function(a, b) {
+//   let c = {}
+//   for (let i = 0; i < a.length; i++){
+//     (c[a[i]]) ? c[a[i]].push(i): c[a[i]] = [i];
+//   }
+//   for (let i = 0; i < a.length; i++){
+//     let d = b - a[i];
+//     if (c[d]){
+//       for(j = 0; j < c[d].length; j++ ){
+//         if (c[d][j] !== i) return [i, c[d][j]];
+//       }
+//     }
+//   }
+//   return false;
+// };
 
 console.log(twoSum([2,7,7,11,15],9))
