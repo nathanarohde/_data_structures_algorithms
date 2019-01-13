8b68770bd598eaa3555cdebe2bf3ed1d3c86eb32
Nathan <nathanarohde@gmail.com>
2019-01-12 16:45:59 -0800

    linear_search or indexOf
    O(n)

---
+++ algorithms/searching_algorithms/linear_search.js
@@ -0,0 +1,9 @@
+function linearSearch(arr, target){
+  for (let i=0; i<arr.length; i++){
+    if (arr[i] === target) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch([5,8,1,100,12,3,12], 12));
+// indexOf
