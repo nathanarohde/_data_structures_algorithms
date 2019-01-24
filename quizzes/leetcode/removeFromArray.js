c7c9687d9078b8a09f15842b2190e7035e370e98
Nathan <nathanarohde@gmail.com>
2019-01-24 15:05:17 -0800

    Create removeFromArray.js
    Array is passed by reference and modified in place which creates some odd behaviors.

---
+++ quizzes/leetcode/removeFromArray.js
@@ -0,0 +1,15 @@
+var removeDuplicates = function(nums) {
+    // return [...new Set(nums)];
+     if (nums.length === 0) return 0;
+     let i = 0;
+     let a = nums[0]
+     for (let j = 1; j < nums.length; j++){
+         if (nums[j] != nums[i]){
+             i++;
+             nums[i] = nums[j];
+         }
+     }
+     return i + 1;
+};
+
+console.log(removeDuplicates([1,2,2,3,3,3,4]));
